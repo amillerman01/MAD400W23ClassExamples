df90041f1d6eaa1d30acf6f40e931f1dc244931c
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-01-10 18:59:21 -0500

    Made the first angular project
    Also added some css to to the app component scss file, to show that css does not leak out of components by default

---
+++ learning-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'We are learning javascript right meow and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then';
+}
