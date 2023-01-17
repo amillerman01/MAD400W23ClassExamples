24410776f3ad83e2a92d5d9abb1f9a9db4a4c60f
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-01-17 18:01:45 -0500

    Added some examples of variables in the app component

--- learning-angular/src/app/app.component.ts
+++ learning-angular/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'We are learning javascript right meow and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then';
+  title?: string; // optional variable, default value is undefined
+
+  constructor() {
+
+    this.title = 'We are learning javascript right meow and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then and then';
+
+    var name = "Peter";
+    let changingName = "Peter";
+    changingName = "Darren";
+
+    const programChair = "Ruth";
+    // programChair = "Not Ruth";
+
+    // can't do this cause it's a string
+    // changingName = 10;
+
+    let programName: string;
+    // let programName: any;
+
+    programName = "MAD400";
+
+    // programName = 10;
+
+  }
 }
