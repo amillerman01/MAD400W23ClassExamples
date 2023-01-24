53fbe48aa0a040e42d1bb2584070508167996765
amillerman <amillerman01@stclaircollege.ca>
2023-01-24 18:18:33 -0500

    Added a templated styling for the contentCard

--- learning-angular/src/app/content-card/content-card.component.ts
+++ learning-angular/src/app/content-card/content-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IContent } from '../models/icontent';
 
 @Component({
   selector: 'app-content-card',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
+  singleCakeItem: IContent;
 
+  constructor() {
+    this.singleCakeItem = {
+      id: 1025,
+      imageUrl:
+        'https://angular.io/assets/images/logos/angular/angular.png',
+      type: "news",
+      body: '<p>This is the body of the content</p>'
+    };
+  }
 }
