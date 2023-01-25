36611c2735101d336fdd468fdb89976bd5ab90c3
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-01-24 21:01:23 -0500

    Added the ngFor to our ContentList
    Instead of manually creating a ContentCard for each IContent in our listOfCakes, we can use what is essentially a foreach loop in the html of the component

--- learning-angular/src/app/content-list/content-list.component.ts
+++ learning-angular/src/app/content-list/content-list.component.ts
@@ -12,10 +12,34 @@ export class ContentListComponent {
   constructor() {
     this.listOfCakes = [];
     this.listOfCakes.push({
+      id: 1024,
+      imageUrl:
+        'https://angular.io/assets/images/logos/angular/angular.png',
+      // type: "news",
+      body: '<p>This is the body of the content</p>'
+    },{
       id: 1025,
       imageUrl:
         'https://angular.io/assets/images/logos/angular/angular.png',
       // type: "news",
+      body: '<p>This is the body of the content</p>'
+    },{
+      id: 1026,
+      imageUrl:
+        'https://angular.io/assets/images/logos/angular/angular.png',
+      type: "Semi Sweet",
+      body: '<p>This is the body of the content</p>'
+    },{
+      id: 1027,
+      imageUrl:
+        'https://angular.io/assets/images/logos/angular/angular.png',
+      // type: "news",
+      body: '<p>This is the body of the content</p>'
+    },{
+      id: 1028,
+      imageUrl:
+        'https://angular.io/assets/images/logos/angular/angular.png',
+      type: "Super sweet",
       body: '<p>This is the body of the content</p>'
     });
   }
