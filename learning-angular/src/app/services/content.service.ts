57e0d03d3bb112b0a34e2425c02cb6f9e75feb4c
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-01-31 18:54:23 -0500

    Added a content service to the project
    using the console command:
    npm run ng generate service services/Content

---
+++ learning-angular/src/app/services/content.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { CONTENT } from '../data/mock-content-db';
+import { IContent } from '../models/icontent';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContentService {
+
+  constructor() { }
+
+}
