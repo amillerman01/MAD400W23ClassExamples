875fbc9927336232fb080b66c593f4fc3eacefd0
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-01-31 19:58:36 -0500

    Completed step 12 of assignment 1
    Be sure to read the comments if you don't understand how something I used works

--- A_Millerman_MyFavouriteNintendoGames/src/app/models/content-list.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/models/content-list.ts
@@ -21,7 +21,43 @@ export class ContentList {
   arrayLength(): number{
     return this._videoGames.length;
   }
+
+  toString(index: number): string{
+    const videoGame: IContent = this._videoGames[index];
+    let prettyHtmlOutput = `
+      <h2>${videoGame.title}</h2>
+      <img src="${videoGame.imgSrc}">
+      <p>${videoGame.description}</p>
+      <span class="author">${videoGame.author}</span>
+      <span class="type">${videoGame.type}</span>`; // this whole string started on line 27!
+
+    if (videoGame.tags) // if our tags array exists
+    {
+      prettyHtmlOutput += '<div class="tags">'; // add a wrapper around the tags that are going to be added
+
+      // I searched on google for a way to use foreach in javascript, and this is what came up
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+      videoGame.tags?.forEach((tag) => {
+        prettyHtmlOutput += `<span>${tag}</span>`; // add each tag to the html output
+      });
+
+      prettyHtmlOutput += "</div>"; // close the wrapper around the tags that were added
+    }
+    return prettyHtmlOutput;
+  }
 }
+
+/*
+Fun fact - This weird anonymous function syntax from lines 40 - 42:
+
+      () => {
+
+      }
+
+This is known as an Arrow Function. You can learn more about it here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+In C# and Java, you might know it better as a Lambda Expression
+*/
+
 
 // example of what it looks like when we use the getter on a ContentList object
 // let games = new ContentList();
