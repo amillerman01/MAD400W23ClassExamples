d1d668ba19e8f491313bbd260a1c96ceaad7ce8c
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-01-31 19:41:10 -0500

    Completed third bullet point of step 11
    Added a getter method, and commented out code for a setter and a usage of the getter, as an example

--- A_Millerman_MyFavouriteNintendoGames/src/app/models/content-list.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/models/content-list.ts
@@ -5,4 +5,16 @@ export class ContentList {
   constructor() {
     this._videoGames = [];
   }
+  get videoGames(): IContent[]{
+    return this._videoGames;
+  }
+
+  // Don't need the setter, but if we did it would look something like this
+  // set videoGames(g: IContent[]){
+  //   this._videoGames = g;
+  // }
 }
+
+// example of what it looks like when we use the getter on a ContentList object
+// let games = new ContentList();
+// console.log(games.videoGames);
