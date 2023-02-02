cc3f8556fe468ff5dbed3c8609ca771b972510dd
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-01 19:35:20 -0500

    Completed step 9 of assignment 2
    Added a description about why we made our input value optional with the question mark in the content list item ts file.

--- A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item/content-list-item.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item/content-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IContent } from '../models/icontent';
 
 @Component({
   selector: 'app-content-list-item',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list-item.component.scss']
 })
 export class ContentListItemComponent {
+  // I can choose to set this as an optional variable with the question mark
+  @Input() contentItemInputValue?: IContent;
 
+  // OR I could use a constructor to give it a default value that will be replaced later
+    /*
+     notice I only set the values of some of the IContent properties, specifically the ones that were not optional.
+     This solution is a little clunky though, as now there will be false data there before the input value is passed in.
+     This can cause unexpected visual problems on the HTML side of things, so we will just set our contentItemInputValue to be optional instead of using this.
+     */
+  // constructor() {
+  //   this.contentItemInputValue = {
+  //     id: -1,
+  //     title: "",
+  //     author: "",
+  //     type: ""
+  //   };
+  // }
 }
