6838243bcea0123538ce947ee51d27997615a3dc
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-07 18:23:07 -0500

    Added rules for accessing the new pages

--- learning-angular/src/app/app-routing.module.ts
+++ learning-angular/src/app/app-routing.module.ts
@@ -2,12 +2,28 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentListComponent } from './content-list/content-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
 
 const routes: Routes = [
   {
+    path: "",
+    redirectTo: "content",
+    pathMatch: "full",
+  },
+  {
     path: "content",
     component: ContentListComponent,
+  },
+  {
+    path: "content/:id",
+    component: ContentDetailComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
   }
+
 ];
 @NgModule({
   declarations: [],
