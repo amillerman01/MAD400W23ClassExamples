fd09381a8ff525d23aa01b38e4d410acc56f2f74
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-07 17:39:32 -0500

    Added necessary routerModule and route rules

--- learning-angular/src/app/app-routing.module.ts
+++ learning-angular/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentListComponent } from './content-list/content-list.component';
 
+const routes: Routes = [
+  {
+    path: "content",
+    component: ContentListComponent,
+  }
+];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
