eb79e461245a675dd2f68707c453adf7ebfc3268
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-08 17:46:03 -0500

    Completed steps 7 and 8 of assignment 3

--- A_Millerman_MyFavouriteNintendoGames/src/app/content-list/content-list.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/content-list/content-list.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IContent } from '../models/icontent';
+import { VideoGameService } from '../service/video-game.service';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
+export class ContentListComponent implements OnInit{
   videoGameArray: IContent[];
 
-  constructor() {
-    // this.videoGameArray =
+  constructor(private videoGameService: VideoGameService) {
+    this.videoGameArray = [];
+  }
+
+  ngOnInit(): void {
+    this.videoGameService.getContent().subscribe((videoGames: IContent[]) => {
+      this.videoGameArray = videoGames;
+    });
   }
 }
