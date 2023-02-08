2bb07dcd535053444f88e4b6bd5ff7cc1476a094
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-08 18:39:10 -0500

    Completed step 12 of assignment 3

--- A_Millerman_MyFavouriteNintendoGames/src/app/app.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IContent } from './models/icontent';
 import { VideoGameService } from './service/video-game.service';
 
 @Component({
@@ -6,10 +7,19 @@ import { VideoGameService } from './service/video-game.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  // singleGame: IContent | undefined;
+  singleGame?: IContent;
   title = 'A_Millerman_MyFavouriteNintendoGames';
 
   constructor(private videoGameService: VideoGameService) {
   }
 
+  ngOnInit(): void{
+    this.videoGameService.getContentItem(3).subscribe((videoGame: IContent) => {
+      // console.log("Testing getting a single content item: ", videoGame);
+       this.singleGame = videoGame;
+    });
+
+  }
 }
