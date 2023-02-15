53e69f8f902e733655049caa43d317929bfae585
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-15 18:47:17 -0500

    Completed step 12 of assignment 4
    This is just one possible way you could have done this

--- A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item-search/content-list-item-search.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item-search/content-list-item-search.component.ts
@@ -10,8 +10,10 @@ import { VideoGameService } from '../service/video-game.service';
 export class ContentListItemSearchComponent implements OnInit {
   // singleGame: IContent | undefined;
   singleGame?: IContent;
+  searchDidNotWork: boolean;
 
   constructor(private videoGameService: VideoGameService) {
+    this.searchDidNotWork = false;
   }
 
   ngOnInit(): void{
@@ -23,6 +25,13 @@ export class ContentListItemSearchComponent implements OnInit {
     console.warn("Getting an item: ", newIdNumber);
 
     this.videoGameService.getContentItem(Number(newIdNumber)).subscribe((videoGame: IContent) => {
+
+      if (videoGame == undefined || videoGame.id === -1) {
+        // an id of -1 is what my INVALIDGAME uses as its id in the mock-content
+        this.searchDidNotWork = true;
+        return;
+      }
+      this.searchDidNotWork = false;
       console.warn("Got the item: ", videoGame);
        this.singleGame = videoGame;
     });
