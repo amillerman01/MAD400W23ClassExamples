6cc51323dd3accf2d5f807962b5b5bbb51e7e5ad
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-15 18:24:49 -0500

    Completed step 10 of assignment 4
    Note I removed the old search code, since we're going to add it in the next step

--- A_Millerman_MyFavouriteNintendoGames/src/app/app.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { IContent } from './models/icontent';
-import { VideoGameService } from './service/video-game.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  // singleGame: IContent | undefined;
-  singleGame?: IContent;
+export class AppComponent {
   title = 'A_Millerman_MyFavouriteNintendoGames';
 
-  constructor(private videoGameService: VideoGameService) {
-  }
-
-  ngOnInit(): void{
-    this.getTheNewItem("3");
-  }
-  getTheNewItem(newIdNumber: string): void {
-    console.warn("Getting an item: ", newIdNumber);
-    this.videoGameService.getContentItem(Number(newIdNumber)).subscribe((videoGame: IContent) => {
-      console.warn("Got the item: ", videoGame);
-       this.singleGame = videoGame;
-    });
-
-  }
 }
--- A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item-search/content-list-item-search.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item-search/content-list-item-search.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IContent } from '../models/icontent';
+import { VideoGameService } from '../service/video-game.service';
 
 @Component({
   selector: 'app-content-list-item-search',
   templateUrl: './content-list-item-search.component.html',
   styleUrls: ['./content-list-item-search.component.scss']
 })
-export class ContentListItemSearchComponent {
+export class ContentListItemSearchComponent implements OnInit {
+  // singleGame: IContent | undefined;
+  singleGame?: IContent;
+
+  constructor(private videoGameService: VideoGameService) {
+  }
+
+  ngOnInit(): void{
+    this.videoGameService.getContentItem(3).subscribe((videoGame: IContent) => {
+      console.warn("Got the item: ", videoGame);
+       this.singleGame = videoGame;
+    });
+
+  }
 
 }
