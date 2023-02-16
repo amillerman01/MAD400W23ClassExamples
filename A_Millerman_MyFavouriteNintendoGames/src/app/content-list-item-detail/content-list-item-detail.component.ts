6b0277bdfc0121298f448e4fe84fba72e7b6d8ab
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-15 19:04:47 -0500

    Completed the first bullet point of step 14

--- A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item-detail/content-list-item-detail.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/content-list-item-detail/content-list-item-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IContent } from '../models/icontent';
+import { VideoGameService } from '../service/video-game.service';
 
 @Component({
   selector: 'app-content-list-item-detail',
   templateUrl: './content-list-item-detail.component.html',
   styleUrls: ['./content-list-item-detail.component.scss']
 })
-export class ContentListItemDetailComponent {
+export class ContentListItemDetailComponent implements OnInit {
+  id?: number;
+  videoGameDetailItem?: IContent;
 
+  constructor(private route: ActivatedRoute,
+    private videoGameService: VideoGameService) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get('idNumberValue'));
+
+
+      this.videoGameService.getContentItem(this.id).subscribe((videoGame: IContent) => {
+        console.warn("Got the item in the detail: ", videoGame);
+         this.videoGameDetailItem = videoGame;
+      });
+
+    });
+  }
 }
