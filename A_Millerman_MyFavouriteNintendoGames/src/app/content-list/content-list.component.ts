fe6cdb8551a293f2c6381d565b7dafdd0d9b01a8
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-02-15 21:41:21 -0500

    Removed the code that tested the service
    It was causing unexpected behaviours when going to the content list, like removing the last element in the array

--- A_Millerman_MyFavouriteNintendoGames/src/app/content-list/content-list.component.ts
+++ A_Millerman_MyFavouriteNintendoGames/src/app/content-list/content-list.component.ts
@@ -37,23 +37,23 @@ export class ContentListComponent implements OnInit{
     //   console.log("Testing adding a game to the array: ", videoGames);
     // });
 
-    let testGameToUpdate: IContent = {
-      id: 1, // this means we're replacing WiiSports with this new game, cause it has the same id
-      title: "The Last Of Us",
-      description: "Some random guy takes on responsibility in a zombie game",
-      author: "Naughty Dog",
-      imgSrc: "https://upload.wikimedia.org/wikipedia/en/4/46/Video_Game_Cover_-_The_Last_of_Us.jpg",
-      type: "zombie",
-    };
+    // let testGameToUpdate: IContent = {
+    //   id: 1, // this means we're replacing WiiSports with this new game, cause it has the same id
+    //   title: "The Last Of Us",
+    //   description: "Some random guy takes on responsibility in a zombie game",
+    //   author: "Naughty Dog",
+    //   imgSrc: "https://upload.wikimedia.org/wikipedia/en/4/46/Video_Game_Cover_-_The_Last_of_Us.jpg",
+    //   type: "zombie",
+    // };
 
-    this.videoGameService.updateContentItem(testGameToUpdate).subscribe((videoGames: IContent[]) => {
-      console.log("Testing updating a game in the array - Wii Sports should be replaced: ", videoGames);
-    });
+    // this.videoGameService.updateContentItem(testGameToUpdate).subscribe((videoGames: IContent[]) => {
+    //   console.log("Testing updating a game in the array - Wii Sports should be replaced: ", videoGames);
+    // });
 
 
-    this.videoGameService.deleteContentItem(3).subscribe((videoGame: IContent) => {
-      console.log("Testing deleting a single item (goose game): ", videoGame);
-    });
+    // this.videoGameService.deleteContentItem(3).subscribe((videoGame: IContent) => {
+    //   console.log("Testing deleting a single item (goose game): ", videoGame);
+    // });
 
 
   }
