951f1e96c605d159e952884688624f661208031f
amillerman <90463478+amillerman01@users.noreply.github.com>
2023-03-07 17:44:29 -0500

    Added the service for the Database creation

---
+++ learning-angular/src/app/services/in-memory-data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { CONTENT } from '../data/mock-content-db';
+import { IContent } from '../models/icontent';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  constructor() { }
+
+  createDb() {
+    return {
+      CONTENT
+    };
+  }
+
+  genId(cakes: IContent[]): number {
+    if (cakes.length > 0) {
+      let cakeIds: number[] = [];
+      cakes.forEach((individualCake) => {
+        cakeIds.push(individualCake.id);
+      })
+
+
+      // using the spread operator
+      return Math.max(...cakeIds) + 1;
+
+      // return Math.max(...cakes.map(c => c.id)) + 1;
+    }
+
+    return 0;
+
+    // return cakes.length > 0 ?
+    //   Math.max(...cakes.map(c => c.id)) + 1 : 0;
+  }
+}
